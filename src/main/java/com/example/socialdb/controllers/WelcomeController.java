93e76622f6379714e3be6f234a8764d7aef5bda6
shevkosasha <shevkosasha@gmail.com>
2024-02-14 23:24:25 +0300

    renamed

--- src/main/java/com/example/socialdb/controllers/WelcomeController.java
+++ src/main/java/com/example/socialdb/controllers/WelcomeController.java
@@ -1,4 +1,4 @@
-package com.example.springlearnkindsonthegenius.controllers;
+package com.example.socialdb.controllers;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
